98e3388f871237f9a86a1a5e846fa870852159d3
송지율 <lnacles@naver.com>
2023-06-03 23:20:35 +0900

    코드 이해하면서 리팩토링 함

--- Encryption.java
+++ Encryption.java
@@ -69,37 +69,38 @@ public class Encryption {
     public void input() {
         Scanner scan = new Scanner(System.in);
         System.out.println("암호화 문자를 한 글자씩 입력하시요(Only English)");
-        this.text = scan.nextLine();
-        this.message = this.text.toCharArray();
+        text = scan.nextLine();
+        message = text.toCharArray();
 
         int i;
-        for(i = 0; i < this.message.length; ++i) {
-            System.out.print("[" + this.message[i] + "] : 암호화할 문자입니다");
+        for(i = 0; i < message.length; ++i) {
+            System.out.println("[" + message[i] + "] : 암호화할 문자입니다");
         }
 
-        for(i = 0; i < this.message.length; ++i) {
-            this.charToInt.add(Integer.valueOf(this.message[i]));
+        for(i = 0; i < message.length; ++i) {
+            charToInt.add(Integer.valueOf(message[i]));
         }
 
         System.out.println();
-        System.out.println(this.charToInt + "암호화 할 문자의 10진수입니다");
+        System.out.println(charToInt + "암호화 할 문자의 10진수입니다");
 
-        for(i = 0; i < this.charToInt.size(); ++i) {
-            if ((Integer)this.charToInt.get(i) == 32) {
-                this.strToInt.add("00" + Integer.toBinaryString((Integer)this.charToInt.get(i)));
+        for(i = 0; i < charToInt.size(); ++i) {
+            /* 32 는 띄어쓰기*/
+            if ((Integer)charToInt.get(i) == 32) {
+                strToInt.add("00" + Integer.toBinaryString((Integer)charToInt.get(i)));
             } else {
-                this.strToInt.add("0" + Integer.toBinaryString((Integer)this.charToInt.get(i)));
+                strToInt.add("0" + Integer.toBinaryString((Integer)charToInt.get(i)));
             }
         }
 
-        System.out.println(this.strToInt + "암호화 문자의 2진수");
+        System.out.println(strToInt + "암호화 문자의 2진수");
         String a = "";
 
-        for(i = 0; i < this.strToInt.size(); ++i) {
-            a = (String)this.strToInt.get(i);
+        for(i = 0; i < strToInt.size(); ++i) {
+            a = (String)strToInt.get(i);
 
             for(int j = 0; j < a.length(); ++j) {
-                this.d.add(Integer.parseInt(String.valueOf(a.charAt(j))));
+                d.add(Integer.parseInt(String.valueOf(a.charAt(j))));
             }
         }
 
@@ -108,12 +109,12 @@ public class Encryption {
     public void cutting() {
         int i;
         for(i = 0; i < 8; ++i) {
-            this.mat.add((Integer)this.d.get(i));
+            mat.add((Integer)d.get(i));
         }
 
-        if (this.d.size() > 0) {
+        if (d.size() > 0) {
             for(i = 0; i < 8; ++i) {
-                this.d.remove(0);
+                d.remove(0);
             }
         }
 
@@ -122,110 +123,110 @@ public class Encryption {
     public void process() {
         int i;
         int j;
-        for(i = 0; i < this.mat.size(); ++i) {
-            for(j = 1; j < this.IP.length + 1; ++j) {
-                if (this.IP[i] == j) {
-                    this.IP_mat.add((Integer)this.mat.get(j - 1));
+        for(i = 0; i < mat.size(); ++i) {
+            for(j = 1; j < IP.length + 1; ++j) {
+                if (IP[i] == j) {
+                    IP_mat.add((Integer)mat.get(j - 1));
                 }
             }
         }
 
-        System.out.println(this.IP_mat + " IP행렬을 거친 암호문");
+        System.out.println(IP_mat + " IP행렬을 거친 암호문");
 
-        for(i = 0; i < this.IP_mat.size(); ++i) {
-            for(j = 1; j < this.EP.length / 2 + 1; ++j) {
-                if (this.EP[i] == j) {
-                    this.EP_mat.add((Integer)this.IP_mat.get(3 + j));
+        for(i = 0; i < IP_mat.size(); ++i) {
+            for(j = 1; j < EP.length / 2 + 1; ++j) {
+                if (EP[i] == j) {
+                    EP_mat.add((Integer)IP_mat.get(3 + j));
                 }
             }
         }
 
         System.out.println("EP는 EP행렬을 거친 암호문입니다");
-        System.out.println(this.EP_mat + " : EP를거친 암호문 ");
-        System.out.println(this.k.P8_key_k1 + " KEY1입니다");
+        System.out.println(EP_mat + " : EP를거친 암호문 ");
+        System.out.println(k.P8_key_k1 + " KEY1입니다");
         System.out.println("EP와 KEY1의 xor연산");
 
-        for(i = 0; i < this.EP_mat.size(); ++i) {
-            if (this.EP_mat.get(i) == this.k.P8_key_k1.get(i)) {
-                this.S_mat.add(0);
+        for(i = 0; i < EP_mat.size(); ++i) {
+            if (EP_mat.get(i) == k.P8_key_k1.get(i)) {
+                S_mat.add(0);
             } else {
-                this.S_mat.add(1);
+                S_mat.add(1);
             }
         }
 
-        System.out.println(this.S_mat + "EP_mat과 p8.key_k1의 xor연산결과");
+        System.out.println(S_mat + "EP_mat과 p8.key_k1의 xor연산결과");
         System.out.println("S_box 시작");
-        this.s_box(this.S_mat);
-        System.out.println(this.Encry + "S_BOX의 결과");
+        s_box(S_mat);
+        System.out.println(Encry + "S_BOX의 결과");
 
-        for(i = 0; i < this.IP_mat.size() / 2; ++i) {
-            if (this.IP_mat.get(i) == this.Encry.get(i)) {
-                this.Encry_result.add(0);
+        for(i = 0; i < IP_mat.size() / 2; ++i) {
+            if (IP_mat.get(i) == Encry.get(i)) {
+                Encry_result.add(0);
             } else {
-                this.Encry_result.add(1);
+                Encry_result.add(1);
             }
         }
 
         for(i = 4; i < 8; ++i) {
-            this.Encry_result.add((Integer)this.IP_mat.get(i));
+            Encry_result.add((Integer)IP_mat.get(i));
         }
 
-        System.out.println(this.Encry_result + " : Switch 전");
+        System.out.println(Encry_result + " : Switch 전");
 
         for(i = 0; i < 4; ++i) {
-            Collections.swap(this.Encry_result, i, i + 4);
+            Collections.swap(Encry_result, i, i + 4);
         }
 
-        System.out.println(this.Encry_result + " : Switch 후 ");
+        System.out.println(Encry_result + " : Switch 후 ");
 
-        for(i = 0; i < this.Encry_result.size(); ++i) {
-            for(j = 1; j < this.EP.length / 2 + 1; ++j) {
-                if (this.EP[i] == j) {
-                    this.Switch_EP_mat.add((Integer)this.Encry_result.get(3 + j));
+        for(i = 0; i < Encry_result.size(); ++i) {
+            for(j = 1; j < EP.length / 2 + 1; ++j) {
+                if (EP[i] == j) {
+                    Switch_EP_mat.add((Integer)Encry_result.get(3 + j));
                 }
             }
         }
 
-        System.out.println(this.Switch_EP_mat + " Switch되고 EP행렬을 거친 암호문");
-        System.out.println(this.k.P8_key_k2 + " : KEY2");
+        System.out.println(Switch_EP_mat + " Switch되고 EP행렬을 거친 암호문");
+        System.out.println(k.P8_key_k2 + " : KEY2");
 
-        for(i = 0; i < this.Switch_EP_mat.size(); ++i) {
-            if (this.Switch_EP_mat.get(i) == this.k.P8_key_k2.get(i)) {
-                this.Switch_S_mat.add(0);
+        for(i = 0; i < Switch_EP_mat.size(); ++i) {
+            if (Switch_EP_mat.get(i) == k.P8_key_k2.get(i)) {
+                Switch_S_mat.add(0);
             } else {
-                this.Switch_S_mat.add(1);
+                Switch_S_mat.add(1);
             }
         }
 
         System.out.println("Switch_EP과 KEY2의 xor연산결과");
-        System.out.println(this.Switch_S_mat + "Switch된 S_BOX전");
+        System.out.println(Switch_S_mat + "Switch된 S_BOX전");
         System.out.println("S_box 시작");
-        this.s_box(this.Switch_S_mat);
-        System.out.println(this.Encry_result + " : S_BOX결과");
+        s_box(Switch_S_mat);
+        System.out.println(Encry_result + " : S_BOX결과");
 
-        for(i = 0; i < this.Encry_result.size() / 2; ++i) {
-            if (this.Encry_result.get(i) == this.Encry.get(i)) {
-                this.Switch_Encry_result.add(0);
+        for(i = 0; i < Encry_result.size() / 2; ++i) {
+            if (Encry_result.get(i) == Encry.get(i)) {
+                Switch_Encry_result.add(0);
             } else {
-                this.Switch_Encry_result.add(1);
+                Switch_Encry_result.add(1);
             }
         }
 
         for(i = 4; i < 8; ++i) {
-            this.Switch_Encry_result.add((Integer)this.Encry_result.get(i));
+            Switch_Encry_result.add((Integer)Encry_result.get(i));
         }
 
-        System.out.println(this.Switch_Encry_result);
+        System.out.println(Switch_Encry_result);
 
-        for(i = 0; i < this.Switch_Encry_result.size(); ++i) {
-            for(j = 1; j < this.IP_1.length + 1; ++j) {
-                if (this.IP_1[i] == j) {
-                    this.Star.add((Integer)this.Switch_Encry_result.get(j - 1));
+        for(i = 0; i < Switch_Encry_result.size(); ++i) {
+            for(j = 1; j < IP_1.length + 1; ++j) {
+                if (IP_1[i] == j) {
+                    Star.add((Integer)Switch_Encry_result.get(j - 1));
                 }
             }
         }
 
-        System.out.println(this.Star + " 완성된 암호문 ");
+        System.out.println(Star + " 완성된 암호문 ");
         System.out.println("---------------------------------------");
     }
 
@@ -252,20 +253,20 @@ public class Encryption {
             s0_y = 3;
         }
 
-        int i = this.S0[s0_x][s0_y];
+        int i = S0[s0_x][s0_y];
         System.out.println(i + " i의 값");
         if (i == 0) {
-            this.fk_mat.add(0);
-            this.fk_mat.add(0);
+            fk_mat.add(0);
+            fk_mat.add(0);
         } else if (i == 1) {
-            this.fk_mat.add(0);
-            this.fk_mat.add(1);
+            fk_mat.add(0);
+            fk_mat.add(1);
         } else if (i == 2) {
-            this.fk_mat.add(1);
-            this.fk_mat.add(0);
+            fk_mat.add(1);
+            fk_mat.add(0);
         } else {
-            this.fk_mat.add(1);
-            this.fk_mat.add(1);
+            fk_mat.add(1);
+            fk_mat.add(1);
         }
 
         byte s1_x;
@@ -290,35 +291,35 @@ public class Encryption {
             s1_y = 3;
         }
 
-        int j = this.S1[s1_x][s1_y];
+        int j = S1[s1_x][s1_y];
         System.out.println(j + " : j의 값");
         if (j == 0) {
-            this.fk_mat.add(0);
-            this.fk_mat.add(0);
+            fk_mat.add(0);
+            fk_mat.add(0);
         } else if (j == 1) {
-            this.fk_mat.add(0);
-            this.fk_mat.add(1);
+            fk_mat.add(0);
+            fk_mat.add(1);
         } else if (j == 2) {
-            this.fk_mat.add(1);
-            this.fk_mat.add(0);
+            fk_mat.add(1);
+            fk_mat.add(0);
         } else {
-            this.fk_mat.add(1);
-            this.fk_mat.add(1);
+            fk_mat.add(1);
+            fk_mat.add(1);
         }
 
-        System.out.println(this.fk_mat + "fk_mat");
-        this.Encry.clear();
+        System.out.println(fk_mat + "fk_mat");
+        Encry.clear();
 
-        for(int a = 0; a < this.fk_mat.size(); ++a) {
-            for(int b = 1; b < this.P4.length + 1; ++b) {
-                if (this.P4[a] == b) {
-                    this.Encry.add((Integer)this.fk_mat.get(b - 1));
+        for(int a = 0; a < fk_mat.size(); ++a) {
+            for(int b = 1; b < P4.length + 1; ++b) {
+                if (P4[a] == b) {
+                    Encry.add((Integer)fk_mat.get(b - 1));
                 }
             }
         }
 
-        System.out.println(this.Encry + ":Encry");
-        this.fk_mat.clear();
-        return this.Encry;
+        System.out.println(Encry + ":Encry");
+        fk_mat.clear();
+        return Encry;
     }
 }
